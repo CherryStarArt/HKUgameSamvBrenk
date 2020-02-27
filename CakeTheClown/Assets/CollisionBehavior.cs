e113ecea81896b57f705bd08526304d5cbc30a99
CherryStarArt <slowmotionfacepunch@outlook.com>
2020-02-27 17:01:54 +0100

    Huiswerk opdracht animatie

---
+++ CakeTheClown/Assets/CollisionBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBehavior : MonoBehaviour
+{
+
+    // 1
+    public bool collided;
+
+    //2
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!collided)
+        {
+            collided = true;
+            // 3
+            GameObject clown = gameObject.transform.parent.gameObject;
+            Animator clownAnimator = clown.GetComponent<Animator>();
+            // 4
+            clownAnimator.SetTrigger("hit");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
